c15f5dad4f4b293915fd0a8e24821451b7f82949
Dave Potts <dave@goopot.co.uk>
2020-10-26 21:12:48 +0000

    Attempting to read clipboard and search

--- asda.js
+++ asda.js
@@ -1,7 +1,18 @@
 window.addEventListener("keydown", event => {
-    if (event.ctrlKey && event.code === "KeyY") { 
-        let search_box = document.getElementById("search")
+    if (event.ctrlKey && event.code === "KeyY") { // Focus on the search box
+        const search_box = document.getElementById("search")
         search_box.focus()
         search_box.select()
     }
+    
+    if (event.ctrlKey && event.shiftkey && event.code === "KeyU") { // Focus on the search box and paste
+        const search_box = document.getElementById("search")
+        search_box.focus()
+        search_box.select()
+        navigator.clipboard.readText.then(
+            cliptext => search_box.value = cliptext
+            search_box.dispatchEvent(new KeyboardEvent('keyup',{'key': cliptext.slice(-1)}))
+        )
+    }
+
 })
