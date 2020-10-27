1d693d6337b2e1824764225d531db0389cb71150
Dave Potts <dave@goopot.co.uk>
2020-10-27 21:20:04 +0000

    Working on paste from clipboard

--- asda.js
+++ asda.js
@@ -1,17 +1,19 @@
 window.addEventListener("keydown", event => {
+
     if (event.ctrlKey && event.code === "KeyY") { // Focus on the search box
         const search_box = document.getElementById("search")
         search_box.focus()
         search_box.select()
     }
     
-    if (event.ctrlKey && event.shiftkey && event.code === "KeyU") { // Focus on the search box and paste
+    if (event.ctrlKey && event.shiftKey && event.code === "KeyU") { // Focus on the search box and paste
         const search_box = document.getElementById("search")
         search_box.focus()
         search_box.select()
-        navigator.clipboard.readText.then(
-            cliptext => search_box.value = cliptext
-            search_box.dispatchEvent(new KeyboardEvent('keyup',{'key': cliptext.slice(-1)}))
+        navigator.clipboard.readText().then(cliptext => {	
+			search_box.value = cliptext
+			search_box.dispatchEvent(new KeyboardEvent('keyup',{'key': cliptext.slice(-1)}))
+			}
         )
     }
 
